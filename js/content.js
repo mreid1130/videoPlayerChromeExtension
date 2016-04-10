37d6618d97d8fff99d7eb6dbb8833efdaa40e375
Mark Reid <mark.francis.reid@gmail.com>
2016-04-10 14:58:32 -0700

    moves js styling into css, adds comments in content.js

--- js/content.js
+++ js/content.js
@@ -8,21 +8,12 @@ chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
     var div = document.createElement('div');
     var closeButton = document.createElement('button');
 
+    // set class and attrs with JS
     closeButton.className = 'close-icon';
     closeButton.setAttribute('type', 'reset');
 
+    // add id and mouse events on div
     div.id = "video-container"
-    div.style.height = "450px";
-    div.style.width = "800px";
-    div.style.position = "fixed";
-    div.style.top = "0px";
-    div.style.left = "0px";
-    div.style.zIndex = '1000000000000000000';
-    div.style.border = "none";
-    div.style.display = "hidden";
-    div.style.WebkitTransition = 'background 0.5s linear';
-    div.style.MozTransition = 'background 0.5s linear';
-    div.style.transition = 'background 0.5s linear';
     div.onmouseover = function() {
       div.style.backgroundColor = "rgba(0, 0, 0, .1)";
       closeButton.style.display = 'inline-block';
@@ -32,26 +23,20 @@ chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
       closeButton.style.display = 'hidden';
     };
 
+    // iframe JS
     iframe.id = 'video-player';
     iframe.src = request.source;
     iframe.height = "90%";
     iframe.width = "90%";
-    iframe.style.display = "block";
-    iframe.style.marginLeft = "auto";
-    iframe.style.marginRight = "auto";
-    iframe.style.marginTop = "auto";
-    iframe.style.marginBottom = "auto";
-    iframe.style.position = "absolute";
-    iframe.style.top = "0";
-    iframe.style.left = "0";
-    iframe.style.bottom = "0";
-    iframe.style.right = "0";
 
+    // insert elements into document, add draggable and resizable properties to container
     document.body.insertBefore(div, document.body.firstChild);
     div.appendChild(closeButton);
     div.appendChild(iframe);
     $("#video-container").resizable();
     $("#video-container").draggable();
+
+    // event listeners
     closeButton.addEventListener("click", function(e) {
       document.body.removeChild(document.getElementById('video-container'));
     }, false);
