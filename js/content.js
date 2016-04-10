6bbb4b6a02735057699678ef1111e268591dba51
Mark Reid <mark.francis.reid@gmail.com>
2016-04-10 00:55:04 -0700

    adds resizeable iframe animations, fade in/out on background color to help with visualizing where resize arrows will be, since it's super janky when there's no margin

--- js/content.js
+++ js/content.js
@@ -5,17 +5,43 @@ chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
       document.body.removeChild(existingVideo);
     }
     var iframe = document.createElement('iframe');
+    var div = document.createElement('div');
+    div.id = "video-container"
+    div.style.height = "360px";
+    div.style.width = "640px";
+    div.style.position = "fixed";
+    div.style.top = "0px";
+    div.style.left = "0px";
+    div.style.zIndex = '1000000000000000000';
+    div.style.border = "none";
+    div.style.display = "hidden";
+    div.style.WebkitTransition = 'background 0.5s linear';
+    div.style.MozTransition = 'background 0.5s linear';
+    div.style.transition = 'background 0.5s linear';
+    div.onmouseover = function() {
+      div.style.backgroundColor = "rgba(0, 0, 0, .1)"
+    };
+    div.onmouseleave = function() {
+      div.style.backgroundColor = "rgba(0, 0, 0, 0)"
+    };
     iframe.id = 'video-player';
-    // Must be declared at web_accessible_resources in manifest.json
     iframe.src = request.source;
-    iframe.height = "360px";
-    iframe.width = "640px";
-    iframe.style.position = "fixed";
-    iframe.style.top = "0px";
-    iframe.style.left = "0px";
-    iframe.style.zIndex = '1000000000000000000'
-    console.log(iframe);
-    document.body.insertBefore(iframe, document.body.firstChild);
+    iframe.height = "95%";
+    iframe.width = "95%";
+    iframe.style.display = "block";
+    iframe.style.marginLeft = "auto";
+    iframe.style.marginRight = "auto";
+    iframe.style.marginTop = "auto";
+    iframe.style.marginBottom = "auto";
+    iframe.style.position = "absolute";
+    iframe.style.top = "0";
+    iframe.style.left = "0";
+    iframe.style.bottom = "0";
+    iframe.style.right = "0";
+
+    document.body.insertBefore(div, document.body.firstChild);
+    div.appendChild(iframe);
+    $("#video-container").resizable();
     sendResponse({
       'success': 'done'
     })
